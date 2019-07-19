73b1a371da8ee20fc0876b981b61c89e59d9121d
TennesseeNo7 <sgosk@sigma.ug.edu.pl>
2019-07-19 19:54:29 +0200

    Packages

--- src/com/babydrivers/gui/controllers/Controller.java
+++ src/com/babydrivers/gui/controllers/Controller.java
@@ -1,7 +1,12 @@
 package com.babydrivers.gui.controllers;
 
+import com.babydrivers.coordination.Coordinator;
+
 public class Controller {
 
+    public void initialize() {
+        new Coordinator();
 
+    }
 
 }
